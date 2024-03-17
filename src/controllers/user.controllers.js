7a885f6246b1dcdb3eb9408ec3b94c756010f144
shweta roy <128317014+ShwetaRoy17@users.noreply.github.com>
2024-03-17 21:53:40 +0530

    email file bug resolved

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,8 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { user } from '../models/user.model.js';
-import { sendCustomEmail } from '../utils/email.js';
+import sendEmail from '../utils/email.js';
+
 
 const registerUser = asyncHandler(async(req,res)=>{
     /*
@@ -47,83 +48,20 @@ const registerUser = asyncHandler(async(req,res)=>{
     `phoneNumber: + 91-${phoneNumber}`+
     `address: ${address}`+
     `Regards,\nYour Sensor Team`
-    sendCustomEmail(createdUser.email,"Successful registration to the Community.",message);
+
+    const mailOptions={
+      from:"Welfare Society <welfare.society.com>",
+      to:email,
+      subject:"Welcome to the community.",
+      text:message
+
+    }
+    const info = await sendEmail(mailOptions);
+    if(info){
     return res.status(201).json(new ApiResponse(200,createdUser,"user registered successfully"))
+  }
 })
 
 
+
 export {registerUser};
-
- // Schedule email sending task every 3 hours
-//  cron.schedule('0 */3 * * *', async () => {
-//     try {
-//         // Fetch data from MongoDB
-
-//         const users = await user.find().exec();
-//         // const userEmails = user.map((user) => {return { email:user.email, username:user.userName }});
-//         const data = {temperature:29,humidity:40,gasValue:60};
-
-//         users.forEach(user => {
-           
-
-//             // Send email to all users
-//             transporter.sendMail(mailOptions, (err, info) => {
-//                 if (err) {
-//                     console.error('Error occurred while sending email', err);
-//                 } else {
-//                     console.log('Email sent:', info.response);
-//                 }
-//             });
-//         })
-
-
-//     } catch (err) {
-//         console.error('Error occurred while fetching data from MongoDB', err);
-//     }
-// });
-
-/*
- `Hello ${user.username}, \n\n`+
-        `Here is your latest sensor data:\n` +
-        `Temperature: ${data.temperature}°C\n` +
-        `Humidity: ${data.humidity}%\n` +
-        `Gas Value: ${data.gasValue}\n\n` +
-        `${dangerMessage}\n\n` +
-        `Regards,\nYour Sensor Team`
-*/ 
-
-/*
-const nodemailer = require('nodemailer');
-
-// Email Configuration
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'your-email@gmail.com',
-    pass: 'your-email-password'
-  }
-});
-
-// Function to send customized email
-async function sendCustomEmail({ to, subject, message }) {
-  try {
-    // Prepare email content
-    const mailOptions = {
-      from: 'your-email@gmail.com',
-      to,
-      subject,
-      text: message
-    };
-
-    // Send email
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent:', info.response);
-    return true; // Email sent successfully
-  } catch (error) {
-    console.error('Error occurred while sending email', error);
-    return false; // Email sending failed
-  }
-}
-
-module.exports = { sendCustomEmail };
-*/ 
--- src/utils/email.js
+++ src/utils/email.js
@@ -1,56 +1,60 @@
-import { user } from '../models/user.model.js'
-// import { SensorData } from '../models/air_quality.model.js'
 import nodemailer from 'nodemailer'
-import cron from 'node-cron'
-import { asyncHandler } from './asyncHandler.js'
 import { ApiResponse } from './ApiResponse.js'
 import { ApiError } from './ApiError.js'
+import {google} from 'googleapis'
+import 'dotenv/config';
 
 
+// OAuth2 Credentials
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+)
+
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN
+});
+
+
+
+
+
+// generating access token
+const accessToken = await oauth2Client.getAccessToken();
+
+
+// transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: "smtp.gmail.com",
+  service: 'gmail',
+  host: "smtp.gmail.com",
   port: 465,
-  secure: true,
+  secure: false,
     auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD
+        type:'OAuth2',
+        user:process.env.MY_EMAIL,
+        clientId:process.env.CLIENT_ID,
+        clientSecret:process.env.CLIENT_SECRET,
+        refreshToken:process.env.REFRESH_TOKEN,
+        accessToken:accessToken
     }
 })
 
-
-// danger message
-const dangermessage = (gas) => {
-    if (gasValue > 100) {
-        return 'Danger: High gas levels detected! Please evacuate immediately.';
-      } else if (gasValue > 50) {
-        return 'Warning: Moderate gas levels detected. Take necessary precautions.';
-      } else {
-        return 'No danger detected. Continue normal operations.';
-      }
-}
  
 
-const sendCustomEmail = asyncHandler(async(to,subject,message)=>{
+// email sending function
+const sendEmail = async(emailOptions)=>{
 try{
- // Prepare email content
-
- console.log( process.env.USER_PASSWORD);
-
- const mailOptions = {
-    from: 'shwetaroy1712@gmail.com',
-    to:to,
-    subject:subject,
-    text:message
-};
-console.log("mail Options\n",mailOptions);
-const info = await transporter.sendMail(mailOptions);
+console.log("mail Options\n",emailOptions);
+// sending email 
+const info = await transporter.sendMail(emailOptions);
 console.log('Email sent Successfully..',info.response);
-return new res.status(200).json(ApiResponse(200,"Email send successfully."));
+return info;
 }catch(err){
 console.log("Error occurred while sending the email\n",err);
 throw new ApiError(409,"email not send");
 }
-})
+}
  
-export {sendCustomEmail};
+export default sendEmail;
